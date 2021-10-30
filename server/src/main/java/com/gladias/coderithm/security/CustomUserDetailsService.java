330db4df771c6b759a5fa458444a8a4dae48432e
Gladias <alek.drozdz@gmail.com>
2021-10-31 01:59:10 +0200

    Implement security

---
+++ server/src/main/java/com/gladias/coderithm/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package java.com.gladias.coderithm.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.com.gladias.coderithm.model.UserEntity;
+import java.com.gladias.coderithm.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("User with login: " + login + "does not exist"));
+
+        return UserPrincipal.of(user);
+    }
+}
