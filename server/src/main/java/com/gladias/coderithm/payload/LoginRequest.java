8c158b52d8feec4b1ba41c1f96a6a2e70a9fddf4
Gladias <alek.drozdz@gmail.com>
2021-10-31 01:58:45 +0200

    Add login and register requests payloads

---
+++ server/src/main/java/com/gladias/coderithm/payload/LoginRequest.java
@@ -0,0 +1,11 @@
+package java.com.gladias.coderithm.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginRequest {
+    private String login;
+    private String password;
+}
---
+++ server/src/main/java/com/gladias/coderithm/payload/RegisterRequest.java
@@ -0,0 +1,25 @@
+package java.com.gladias.coderithm.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class RegisterRequest {
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String login;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String passwordConfirm;
+
+    @Email
+    private String email;
+}
